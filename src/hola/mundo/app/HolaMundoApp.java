0c71e2936d094306a4a70c4081fbffc3d48208ca
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2018-01-11 09:32:50 +0100

    Mejorado

--- src/hola/mundo/app/HolaMundoApp.java
+++ src/hola/mundo/app/HolaMundoApp.java
@@ -1,22 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hola.mundo.app;
 
-/**
- *
- * @author lrodriguezbarroncas
- */
 public class HolaMundoApp {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
-   
-        System.out.println("Hola");  
+
+        System.out.println("Hola");
+
+        System.out.println(2+2);// Esto era indispensable para el buen funcionamiento del programa
+
+        // Versión2.0
     }
 }
